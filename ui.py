6b0f98b85e1941d02f97b0846d06b2ead2badf74
charalambosp <papaloizouc@gmail.com>
2013-10-15 00:59:42 +0100

    added a QListWidget on QDialog + tidy up a bit

--- ui.py
+++ ui.py
@@ -1,18 +1,39 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui,QtCore
 import signal
 import time
 
 
 def doSomething():
-    sys.exit(666)
+    sys.exit(-123)
+
+class Dialog(QtGui.QDialog):
+
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        self.setWindowTitle("Title")
+
+        self.qlist = QtGui.QListWidget()
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(self.qlist)
+        self.setLayout(hbox)
+        self.questions = []
+
+    def add_questions(self, questions):
+        self.questions = questions
+        self._add_questions()
+
+    def _add_questions(self):
+        self.qlist.clear()
+        for question in self.questions:
+            self.qlist.addItem(question)
 
 
 class SystemTrayIcon(QtGui.QSystemTrayIcon):
     def __init__(self, icon, parent=None):
         QtGui.QSystemTrayIcon.__init__(self, icon, parent)
         self.dialog = None
-
         self.menu = QtGui.QMenu(parent)
 
         self.exitAction = self.menu.addAction("Exit")
@@ -21,13 +42,15 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
         self.setContextMenu(self.menu)
 
     def clicked(self, *args, **kwargs):
-        if args[0] == 1:#right click
+        if args[0] == 1:#right click is for menu
             return
+
         if self.dialog is not None:#hide if all-ready exists
             self.dialog = None
             return
-        self.dialog = QtGui.QDialog()
-        self.dialog.setWindowTitle("Title")
+
+        self.dialog = Dialog()
+        self.dialog.add_questions(["----","____"])
         self.dialog.show()
 
 
