28792d5beb40a505ba772f0101c91775458f6b15
charalambosp <papaloizouc@gmail.com>
2013-11-30 17:54:00 +0000

    updating "install" files

--- arch.sh
+++ arch.sh
@@ -1,2 +1,5 @@
 pacman -S python3 python-pyqt4 python-pip
 pip3 install selenium
+sudo mkdir -p /usr/lib/sonot
+cp sonot.zip /usr/lib/sonot/sonot.zip
+sudo cp sonot /usr/bin/sonot
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,2 +1,5 @@
 apt-get install python3 python3-pyqt4 python3-pip
 pip3 install selenium
+sudo mkdir -p /usr/lib/sonot
+cp sonot.zip /usr/lib/sonot/sonot.zip
+sudo cp sonot /usr/bin/sonot
