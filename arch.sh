d89335f106f8130a7d120bc2a0e07b2eae25aa1d
charalambosp <papaloizouc@gmail.com>
2013-11-30 18:31:41 +0000

    fixing install scripts( QtIcon wont work with zipfiles accepts path instead of file)

--- arch.sh
+++ arch.sh
@@ -1,5 +1,7 @@
 pacman -S python3 python-pyqt4 python-pip
 pip3 install selenium
-sudo mkdir -p /usr/lib/sonot
-cp sonot.zip /usr/lib/sonot/sonot.zip
+sudo mkdir -p /opt/sonot
+_dir=`pwd`
+cp -r $_dir /opt/sonot/
 sudo cp sonot /usr/bin/sonot
+sudo chmod 755 /opt/sonot/stackoverflow-notifications/sonot.py
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,7 @@
 apt-get install python3 python3-pyqt4 python3-pip
 pip3 install selenium
-sudo mkdir -p /usr/lib/sonot
-cp sonot.zip /usr/lib/sonot/sonot.zip
+sudo mkdir -p /opt/sonot
+_dir=`pwd`
+cp -r _dir /opt/sonot/
+sudo chmod 755 /opt/sonot/stackoverflow-notifications/sonot.py
 sudo cp sonot /usr/bin/sonot
